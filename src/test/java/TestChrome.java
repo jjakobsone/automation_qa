fc6b8cf563ccdeb29c103320c2a7405f42ecce7f
jjakobsone <jakobsone.julija@gmail.com>
2022-11-29 20:20:18 +0200

    [master]: Add classroom progress: 29.11

--- src/test/java/TestChrome.java
+++ src/test/java/TestChrome.java
@@ -2,9 +2,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static lv.acodemy.constants.Generic.GOOGLE_URL;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class TestChrome {
 
@@ -13,13 +18,13 @@ public class TestChrome {
     public static final String GOOGLE_URL = "https://www.google.lv/?hl=en";
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         driver.close();
         driver.quit();
     }
 
-    @Test
-    public void chromeTest(){
+    @Test(enabled = false)
+    public void chromeTest() {
         driver.get(GOOGLE_URL);
 
         WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
@@ -27,11 +32,22 @@ public class TestChrome {
 
         WebElement searchField = driver.findElement(By.name("q"));
         searchField.sendKeys("acodemy");
-
         searchField.sendKeys(Keys.ENTER);
 
         Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
+    }
 
+    @Test (enabled = false)
+    public void chromeTest2() {
+        driver.get(GOOGLE_URL);
 
+        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
+        acceptButton.click();
+
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("acodemy");
+        searchField.sendKeys(Keys.ENTER);
+
+//        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
     }
 }
--- src/test/java/TestSauceDemo.java
+++ src/test/java/TestSauceDemo.java
@@ -1,36 +1,46 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lv.acodemy.constants.Generic;
 import lv.acodemy.page_object.InventoryPage;
 import lv.acodemy.page_object.LoginPage;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static lv.acodemy.constants.Generic.SAUCE_URL;
 
 public class TestSauceDemo {
+
     ChromeDriver driver;
     LoginPage loginPage;
     InventoryPage inventoryPage;
 
-
-    @BeforeMethod (description = "Preconditions")
+    @BeforeMethod(description = "Preconditions")
     public void initialize() {
-        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
         loginPage = new LoginPage(driver);
         inventoryPage = new InventoryPage(driver);
         driver.get(SAUCE_URL);
     }
 
-    @Test (description = "Happy path: test authorixation with standard user")
+
+    @Test(description = "Happy path: Test authorization with standard user")
     public void authorizeTest() {
         loginPage.authorize("standard_user", "secret_sauce");
-        Assert.assertEquals(inventoryPage.itemElementCount(),6);
+        Assert.assertEquals(inventoryPage.itemElementCount(), 6);
     }
 
-    @Test (description = "Failure: test authorization error message with incorrect credentials" )
-    public void invalidCredentialTest(){
+    @Test(description = "Failure: Test authorization error message with incorrect credentials")
+    public void invalidCredentialTest() {
         loginPage.authorize("standard_user", "incorrect_pw");
         Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username and password do not match any user in this service");
     }
