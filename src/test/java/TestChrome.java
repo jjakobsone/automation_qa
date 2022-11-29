e6bbf6108b374de02df62fa26cfcced36a26e938
jjakobsone <jakobsone.julija@gmail.com>
2022-11-29 20:56:33 +0200

    [master]: update testChrome

--- src/test/java/TestChrome.java
+++ src/test/java/TestChrome.java
@@ -2,14 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static lv.acodemy.constants.Generic.GOOGLE_URL;
-import static org.testng.AssertJUnit.assertEquals;
 
 public class TestChrome {
 
